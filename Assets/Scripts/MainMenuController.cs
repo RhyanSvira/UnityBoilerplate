f52350ca3a37bccb2762485112af39126b1f05ef
RhyanSvira <melonman20x6@gmail.com>
2025-10-09 15:26:04 -0400

    test commit

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class MainMenuController : MonoBehaviour
 {
+
+    //load game scene
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
