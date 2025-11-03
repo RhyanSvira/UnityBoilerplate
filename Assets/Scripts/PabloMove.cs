735d423b0d82d7bbb79274469fa27f4a78cf34d1
RhyanSvira <melonman20x6@gmail.com>
2025-11-03 15:16:32 -0500

    pablo
    added a placeholder and a script that will eventually make it move

---
+++ Assets/Scripts/PabloMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PabloMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            rb.velocity = new Vector2(0, 100);
+        if (Input.GetKey(KeyCode.D))
+            rb.velocity = new Vector2(0, -100);
+    }
+}
