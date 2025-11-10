bea24294a724b9b55b31d2559a0c7acef8bc11e5
RhyanSvira <melonman20x6@gmail.com>
2025-11-10 15:46:39 -0500

    move
    pablo moves between three spots

--- Assets/Scripts/PabloMove.cs
+++ Assets/Scripts/PabloMove.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PabloMove : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public Transform left, center, right;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +17,32 @@ public class PabloMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-            rb.velocity = new Vector2(-50, 0);
-        if (Input.GetKey(KeyCode.D))
-            rb.velocity = new Vector2(50, 0);
+        if (Input.GetKey(KeyCode.A)){
+            if (rb.position.x == center.position.x)
+            {
+                transform.position = left.position;
+            }
+            else if (rb.position.x == right.position.x)
+            {
+                transform.position = center.position;
+            }else if (rb.position.x == left.position.x)
+            {
+                transform.position = right.position;
+            }
+        }
+        if (Input.GetKey(KeyCode.D)){
+            if (rb.position.x == center.position.x){
+                transform.position = right.position;
+            } 
+            else if (rb.position.x == left.position.x)
+            {
+                transform.position = center.position;
+            } 
+            else if (rb.position.x == right.position.x)
+            {
+                transform.position = left.position;
+            }
+        }
+
     }
 }
