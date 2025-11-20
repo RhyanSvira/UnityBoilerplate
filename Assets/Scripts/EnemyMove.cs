25de409f71ec0e1629648057ae1184b218837214
RhyanSvira <melonman20x6@gmail.com>
2025-11-20 15:04:52 -0500

    11/20 3:00 pm
    added the bones of a projectile spawner.
    cleaned up enemy code, for the most part

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,23 +1,25 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-//
-// public class EnemyMove : MonoBehaviour
-// {
-//     private Rigidbody2D _enemy;
-//     public float speed=2.0f;
-//     public float leaftBound=-4.0f;
-//     public float rightBound=4.0f;
-//     private int direction=1;//1 for right, -1 for left
-//
-// }
-//     void Update()
-//     {
-//         transform.Translate(Vector3 * speed * direction * Time.deltaTime);
-//         if (transform.position.x >= rightBound) {
-//         direction = -1;
-//     }
-//      else if (transform.position.x <= leftBound) {
-//         direction = 1; 
-//      }
-//  }
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private Rigidbody2D _enemy;
+    public float speed=2.0f;
+    public float leftBound=-4.0f;
+    public float rightBound=4.0f;
+    private int direction=1;//1 for right, -1 for left <--enum?
+    void Update()
+    {
+        transform.Translate(Vector3 * speed * direction * Time.deltaTime);
+        if (transform.position.x >= rightBound)
+        {
+            direction = -1;
+        }
+        else if (transform.position.x <= leftBound) 
+        {
+            direction = 1; 
+        }
+    }
+}
+    
