e74f74d7b6b779dbc19e929f4892b443e164aaf5
Gaige Berry-Cherchio <gaige.berry-cherchio@mymail.champlain.edu>
2025-12-07 21:11:53 -0500

    I can'5t figure out how to make the enemey jump to the next platform

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,23 +1,59 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-//
-// public class EnemyMove : MonoBehaviour
-// {
-//     private Rigidbody2D _enemy;
-//     public float speed=2.0f;
-//     public float leftBound=-4.0f;
-//     public float rightBound=4.0f;
-//     private int direction=1;//1 for right, -1 for left
-//
-// }
-//     void Update()
-//     {
-//         transform.Translate(Vector3 * speed * direction * Time.deltaTime);
-//         if (transform.position.x >= rightBound) {
-//         direction = -1;
-//     }
-//      else if (transform.position.x <= leftBound) {
-//         direction = 1; 
-//      }
-//  }
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Serialization;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D _enemyRigidBody2D;
+    [FormerlySerializedAs("UnitsToMove")] public int unitsToMove = 8;
+    [FormerlySerializedAs("EnemySpeed")] public float enemySpeed = 500;
+    [FormerlySerializedAs("_isFacingRight")] public bool isFacingRight;
+    private float _startPos;
+    private float _endPos;
+
+    [FormerlySerializedAs("_moveRight")] public bool moveRight = true;
+
+
+    // Use this for initialization
+    public void Awake()
+    {
+        _enemyRigidBody2D = GetComponent<Rigidbody2D>();
+        _startPos = transform.position.x;
+        _endPos = _startPos + unitsToMove;
+        isFacingRight = transform.localScale.x > 0;
+    }
+
+
+// Update is called once per frame
+    public void Update()
+    {
+
+        if (moveRight)
+        {
+            _enemyRigidBody2D.AddForce(Vector2.right * (enemySpeed * Time.deltaTime));
+            if (!isFacingRight)
+                Flip();
+        }
+
+        if (_enemyRigidBody2D.position.x >= _endPos)
+            moveRight = false;
+
+        if (!moveRight)
+        {
+            _enemyRigidBody2D.AddForce(-Vector2.right * (enemySpeed * Time.deltaTime));
+            if (isFacingRight)
+                Flip();
+        }
+        if (_enemyRigidBody2D.position.x <= _startPos)
+            moveRight = true;
+
+
+    }
+
+    private void Flip()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        isFacingRight = transform.localScale.x > 0;
+    }
+
+}
